049648746a0d3b4c2a8323531285a36ce0dd068e
Karsten Belt <henkdekipgaming@gmail.com>
2019-05-06 13:09:18 +0200

    .

---
+++ src/Component.java
@@ -0,0 +1,11 @@
+public class Component {
+
+    private int prijs;
+    private String naam;
+    private String type;
+
+    public Component(int prijs, String naam, String type) {
+        this.prijs = prijs;
+        this.naam = naam;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static double uptime = 99.99;
+    public static ArrayList<Component> componenten;
+
+    public static void main(String[] args) {
+        componenten = new ArrayList<>();
+    }
 }
