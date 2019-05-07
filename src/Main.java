b477a92e7f45bb4ea0755d32eebfeac1d298ab60
Karsten Belt <henkdekipgaming@gmail.com>
2019-05-07 15:35:44 +0200

    aanpassinkjes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Berekening b = new Berekening(99.99);
+        Backtracker b = new Backtracker(20, 60, 1, 1);
+        if (b.getBest() != null) {
+            System.out.println("The cheapest solution is: " + b.getBest());
+        } else {
+            System.out.println("No solution was found!");
+        }
     }
 }
